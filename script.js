541af92c07f0e9a855912fb5b0103455b6109b52
HuntOreo <hloreair@outlook.com>
2025-07-15 00:10:56 -0400

    Generate and render the board

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 console.log('Hello, World!');
+
+const createCanvas = () => {
+  const board = document.querySelector('.board');
+  for (let i = 0; i < 16; i++) {
+    const column = document.createElement('div');
+    column.classList.add('column');
+    board.appendChild(column)
+    for (let k = 0; k < 16; k++) {
+      const tile = document.createElement('div');
+      tile.classList.add('tile')
+      const currentElm = board.lastElementChild
+      currentElm.appendChild(tile);
+    }
+  }
+}
+
+createCanvas();
