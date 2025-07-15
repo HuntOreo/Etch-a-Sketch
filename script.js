91e6717bce709a4e45545acbf8bbf344e5aa0f1f
HuntOreo <hloreair@outlook.com>
2025-07-15 01:10:13 -0400

    Click to create custom board

--- script.js
+++ script.js
@@ -1,31 +1,49 @@
-console.log('Hello, World!');
+const btn = document.querySelector('.new-board');
 
-const createCanvas = () => {
+const createCanvas = (rowCount, columnCount) => {
   const board = document.querySelector('.board');
 
   // create and render a 16x16 grid of tiles.
-  for (let i = 0; i < 16; i++) {
+  for (let i = 0; i < rowCount; i++) {
     const row = document.createElement('div');
     row.classList.add('row');
-    board.appendChild(row)
+    board.appendChild(row);
 
-    for (let k = 0; k < 16; k++) {
+    for (let k = 0; k < columnCount; k++) {
       const tile = document.createElement('div');
-      tile.classList.add('tile')
+      tile.classList.add('tile');
 
       tile.addEventListener("mouseover", (e) => {
         const elem = e.target;
         updateColor(elem);
-      })
+      });
 
-      const currentElm = board.lastElementChild
+      const currentElm = board.lastElementChild;
       currentElm.appendChild(tile);
-    }
-  }
-}
+    };
+  };
+};
 
 const updateColor = (elem) => {
   elem.style.backgroundColor = 'blue';
+};
+
+const updateBoard = () => {
+  try {
+    const columnAmount = Number(prompt('How many columns?'));
+    const rowAmount = Number(prompt('How many rows?'));
+
+    if (!columnAmount || !rowAmount) {
+      throw new Error('Only accepts number');
+    };
+
+    createCanvas(rowAmount, columnAmount);
+  } catch (error) {
+    alert(error.message);
+    return;
+  };
 }
 
-createCanvas();
+btn.addEventListener('click', updateBoard);
+
+createCanvas(16, 16);
