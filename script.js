e60370c26f261cd7ccff8b32ca363388a59f3113
HuntOreo <hloreair@outlook.com>
2025-07-15 00:24:25 -0400

    Draw on the board

--- script.js
+++ script.js
@@ -2,17 +2,30 @@ console.log('Hello, World!');
 
 const createCanvas = () => {
   const board = document.querySelector('.board');
+
+  // create and render a 16x16 grid of tiles.
   for (let i = 0; i < 16; i++) {
-    const column = document.createElement('div');
-    column.classList.add('column');
-    board.appendChild(column)
+    const row = document.createElement('div');
+    row.classList.add('row');
+    board.appendChild(row)
+
     for (let k = 0; k < 16; k++) {
       const tile = document.createElement('div');
       tile.classList.add('tile')
+
+      tile.addEventListener("mouseover", (e) => {
+        const elem = e.target;
+        updateColor(elem);
+      })
+
       const currentElm = board.lastElementChild
       currentElm.appendChild(tile);
     }
   }
+}
+
+const updateColor = (elem) => {
+  elem.style.backgroundColor = 'blue';
 }
 
 createCanvas();
